eca2123f732397cfaafa6ea9e3c9bf1239a66951
JhonZafra <jezortiz26@gmail>
2022-05-10 14:23:48 -0500

    Explicacion de Tailwin

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="bg-red-500 text-blue-300">Hola mundo Tailwin</div>
+    </div>
+  );
+}
+
+export default App;
